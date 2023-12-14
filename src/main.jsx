516d244a7136ed7cc265554c5d16580af1761836
Manu Gonzalez <manuel.gonzalez@interactiv4.com>
2023-12-14 12:34:17 +0100

    Vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <BrowserRouter>
           <App />
+          <Analytics />
       </BrowserRouter>
   </React.StrictMode>,
 )
