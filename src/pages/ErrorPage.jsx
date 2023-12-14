152cea3a0f711129e09b0aeb0b94f626a857b0be
Manu Gonzalez <manuel.gonzalez@interactiv4.com>
2023-12-14 17:35:05 +0100

    404 page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,7 +2,6 @@ import { useRouteError } from "react-router-dom";
 
 export function ErrorPage() {
     const error = useRouteError();
-    console.error('uput');
 
     return (
         <div className="d-flex align-items-center justify-content-center vh-100">
@@ -10,7 +9,7 @@ export function ErrorPage() {
                 <h1 className="display-1 fw-bold">404</h1>
                 <p className="fs-3"> <span className="text-danger">Opps!</span> Page not found.</p>
                 <p className="lead">
-                    The page you’re looking for doesn’t exist.
+                    {error.data}
                 </p>
                 <a href="/" className="btn btn-primary">Go Home</a>
             </div>
