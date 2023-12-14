e4b7032699f3beccce8fd73e850c317f38c95389
Manu Gonzalez <manuel.gonzalez@interactiv4.com>
2023-12-14 17:29:11 +0100

    Redesign router of react

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider } from "react-router-dom"
+import App from './App.jsx'
 import './index.css'
-import { Analytics } from '@vercel/analytics/react';
-import { Router } from  './components/Router/Index.jsx';
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-      <RouterProvider router={Router} />
-      <Analytics />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
 )
