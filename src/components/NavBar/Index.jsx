0f04734a6a7f0c0b477002d438c0a16eb1415499
Manu Gonzalez <manuel.gonzalez@interactiv4.com>
2023-12-14 11:13:36 +0100

    Update navBar with effects

--- src/components/NavBar/Index.jsx
+++ src/components/NavBar/Index.jsx
@@ -1,19 +1,54 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation} from 'react-router-dom';
+
 export function NavBar() {
 
+    const location = useLocation().pathname.substring(1);
+    console.log(location)
+    let menuItems = [
+        {
+            'index': 'Home',
+            'url': ''
+        },
+        {
+            'index': 'About us',
+            'url': 'about'
+        },
+        {
+            'index': 'Contact',
+            'url': 'contact'
+        }
+    ]
+
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
+        <header className="d-flex justify-content-center py-3">
+            <ul className="nav nav-pills">
+                {
+                    menuItems.map((menuItem) => {
+                        return (
+                            <>
+                                <li className="nav-item">
+                                    <Link className={ `nav-link ${location == menuItem.url ? 'active' : ''}` } to={menuItem.url}>{menuItem.index}</Link>
+                                </li>
+                            </>
+                        )
+                    })
+                }
             </ul>
-        </nav>
+        </header>
     )
 }
+
+
+/**
+ *             )
+ *
+ *     return menuItems.map((menuItem, index, users) => {
+ *         return (
+ *             <>
+ *                 <div>
+ *                     <span>{menuItem.index}</span>
+ *                 </div>
+ *             </>
+ *         )
+ *     })
+ */
