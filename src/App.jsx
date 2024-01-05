645cf0854e89ebfe7d6b757103e30a4e69320ce6
Manu Gonzalez <manuel.gonzalez@interactiv4.com>
2024-01-05 16:01:05 +0100

    about

--- src/App.jsx
+++ src/App.jsx
@@ -67,8 +67,8 @@ function App() {
                       </div>
                       <div className="d-md-flex nav-cards gap-4 mt-4">
                           <a target="_self" className="p-4 rounded-3 animate__animated  animate__fadeInUp card-delay-2" href="/blog">
-                              <h3 className="h6">Blog</h3>
-                              <p className="">Read what I share with the world</p>
+                              <h3 className="h6">About</h3>
+                              <p className="">Read more about me</p>
                           </a>
                           <a target="_self"  className="p-4 rounded-3 animate__animated  animate__fadeInUp card-delay-3" href="/portfolio">
                               <h3 className="h6">Let's talk</h3>
