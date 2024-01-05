1020ee3829e2bc2c414e098ade365f092502dab3
Manu Gonzalez <manuel.gonzalez@interactiv4.com>
2024-01-06 00:44:54 +0100

    404 page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -7,9 +7,9 @@ export function ErrorPage() {
         <div className="d-flex align-items-center justify-content-center vh-100">
             <div className="text-center">
                 <h1 className="display-1 fw-bold">{error.status}</h1>
-                <p className="fs-3"> <span className="text-advice">Opps!</span> {error.statusText}</p>
-                <p className="lead">{error.data}</p>
-                <a href="/" className="text-advice pt-5">Go Home</a>
+                <p className="fs-3"> <span className="text-advice">Opps!</span> Page {error.statusText}</p>
+                <p className="text-small">{error.data}</p>
+                <a href="/" className="text-advice pt-5">Back to Home</a>
             </div>
         </div>
     );
