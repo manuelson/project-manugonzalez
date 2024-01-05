fc77e4cfa54ed1313a169c1c3d85d9082a5d0682
Manu Gonzalez <manuel.gonzalez@interactiv4.com>
2024-01-06 00:22:16 +0100

    test logo

--- src/components/NavBar/Index.jsx
+++ src/components/NavBar/Index.jsx
@@ -17,11 +17,10 @@ export function NavBar() {
                 <button className="rounded-circle button-menu align-items-center justify-content-center d-flex">
                     <svg
                         className="icon-menu"
-                        viewBox="0 0 24 24" fill="none"
+                        viewBox="0 0 24 24"
                         xmlns="http://www.w3.org/2000/svg"
                     >
-                        <path d="M4 6H20M4 12H20M4 18H20"
-                              strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                        <path d="M4 6H20M4 12H20M4 18H20" strokeWidth="2"/>
                     </svg>
                 </button>
             </div>
