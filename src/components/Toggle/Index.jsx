f537383943ccdea42618ccfc84242006aac7dfb6
Manu Gonzalez <manuel.gonzalez@interactiv4.com>
2024-01-05 15:29:50 +0100

    css

--- src/components/Toggle/Index.jsx
+++ src/components/Toggle/Index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export const LightModeToggle = () => {
     const [isLight, setIsLight] = useState(false);
