388e7170881574e9cfde36d2c81bcac7c581b676
Manu Gonzalez <manuel.gonzalez@interactiv4.com>
2024-01-07 21:16:49 +0100

    Link blank

--- src/components/Banner/Index.jsx
+++ src/components/Banner/Index.jsx
@@ -12,9 +12,9 @@ export function Banner() {
                 </h1>
                 <p className="text-grey justify-content-around text-banner">
                     +11 years of experience in web development.
-                    <a className="text-advice" href="https://php.net/" target='_blank'> PHP</a>,
-                    <a className="text-advice" href="https://reactjs.org/" target='_blank'> React</a> and
-                    <a className="text-advice" href="https://getbootstrap.com/" target='_blank'> Bootstrap CSS</a>
+                    <a className="text-advice" href="https://php.net/" target="_blank"> PHP</a>,
+                    <a className="text-advice" href="https://reactjs.org/" target="_blank"> React</a> and
+                    <a className="text-advice" href="https://getbootstrap.com/" target="_blank"> Bootstrap CSS</a>
                 </p>
             </div>
         </section>
