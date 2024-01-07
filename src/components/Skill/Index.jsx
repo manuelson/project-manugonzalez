3fabd201b8c9512e4f2ec50bc99b88f677eba440
Manu Gonzalez <manuel.gonzalez@interactiv4.com>
2024-01-07 21:20:48 +0100

    px for mobile

--- src/components/Skill/Index.jsx
+++ src/components/Skill/Index.jsx
@@ -6,7 +6,7 @@ export function Skill({icon, text}) {
         return (
             <li className="skill-card rounded-3 p-3 col">
                 {icon}
-                <span className="justify-content-center px-3">{text}</span>
+                <span className="justify-content-center px-sm-1 px-md-3 px-lg-3 px-xl-3 px-xxl-3">{text}</span>
             </li>
         )
     }
