999ce258281d01b245a85dbc5e3691be94a98233
Manu Gonzalez <manuel.gonzalez@interactiv4.com>
2024-01-07 16:44:17 +0100

    speedInsights

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
