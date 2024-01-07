b6d1bf1b9e87ed8fe9c31e4c557593a8ce43ebfb
Manu Gonzalez <manuel.gonzalez@interactiv4.com>
2024-01-07 20:54:52 +0100

    Fix grid system of skills

--- src/components/Skill/Index.jsx
+++ src/components/Skill/Index.jsx
@@ -2,12 +2,18 @@ import PropTypes from 'prop-types';
 
 export function Skill({icon, text}) {
 
-    return (
-        <li className="skill-card rounded-3 p-3 col">
-            {icon}
-            <span className="justify-content-center px-3">{text}</span>
-        </li>
-    )
+    if (icon && text) {
+        return (
+            <li className="skill-card rounded-3 p-3 col">
+                {icon}
+                <span className="justify-content-center px-3">{text}</span>
+            </li>
+        )
+    } else {
+        return (
+            <li className="rounded-3 p-3 col"></li>
+        )
+    }
 }
 
 Skill.propTypes = {
--- src/components/Skills/Index.jsx
+++ src/components/Skills/Index.jsx
@@ -18,25 +18,26 @@ export function Skills() {
                 Skills
             </h2>
             <hr />
-            <ul className="row gap-3 mx-0 my-0">
+            <h2 className="text-small mt-4">Frontend</h2>
+            <ul className=" mx-0 my-0 skills">
                 <Skill icon={<ReactSvg/>} text='React' />
                 <Skill icon={<JavascriptSvg/>} text='Javascript' />
                 <Skill icon={<TypescriptSvg/>} text='Typescript' />
-            </ul>
-            <ul className="row gap-3 mt-3 mx-0 my-0">
-                <Skill icon={<PhpSvg/>} text='PHP' />
                 <Skill icon={<BootstrapSvg/>} text='Bootstrap' />
-                <Skill icon={<HtmlSvg/>} text='Html' />
-            </ul>
-            <ul className="row gap-3 mt-3  mx-0 my-0">
-                <Skill icon={<CssSvg/>} text='CSS' />
                 <Skill icon={<TailwindSvg/>} text='Tailwind' />
-                <Skill icon={<GithubSvg/>} text='Github' />
+                <Skill icon={<HtmlSvg/>} text='HTML' />
+                <Skill icon={<CssSvg/>} text='CSS' />
+                <Skill icon='' text='' />
+                <Skill icon='' text='' />
             </ul>
-            <ul className="row gap-3 mt-3  mx-0 my-0">
+            <h2 className="text-small mt-4">Backend</h2>
+            <ul className=" mx-0 my-0 skills">
+                <Skill icon={<PhpSvg/>} text='PHP' />
+                <Skill icon={<GithubSvg/>} text='Github' />
                 <Skill icon={<LaravelSvg/>} text='Laravel' />
-                <Skill icon={<MagentoSvg/>} text='Magento e-commerce' />
+                <Skill icon={<MagentoSvg/>} text='Magento' />
                 <Skill icon={<MysqlSvg/>} text='Mysql' />
+                <Skill icon='' text='' />
             </ul>
         </section>
     )
