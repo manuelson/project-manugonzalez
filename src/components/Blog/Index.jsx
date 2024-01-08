287a1f32ab2169db8e8507b8d5e146bb11b8c1ff
Manu Gonzalez <manuel.gonzalez@interactiv4.com>
2024-01-08 10:40:16 +0100

    blog layer

---
+++ src/components/Blog/Index.jsx
@@ -0,0 +1,16 @@
+
+export function Blog() {
+
+    return (
+        <section id="blog" className="container mt-5">
+            <h2 className="h6">Blog</h2>
+            <hr />
+            <div className="d-grid gap-4 blog ">
+                <div className="d-flex align-items-center blog-box w-100 justify-content-center">
+                    <span className="text-advice">Comming soon</span>
+                </div>
+
+            </div>
+        </section>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import {Banner} from "../components/Banner/Index.jsx";
 import {Skills} from "../components/Skills/Index.jsx";
 import {Link} from "react-router-dom";
+import {Blog} from "../components/Blog/Index.jsx";
 
 export function Home() {
 
@@ -11,42 +12,7 @@ export function Home() {
     return (
         <>
             <Banner />
-            <section id="pages" className="container mt-5">
-                <h2 className="h6">Pages</h2>
-                <hr />
-                <div className="d-md-flex nav-cards gap-4 ">
-                    <Link
-                        className="p-4 rounded-3 animate__animated  animate__fadeInUp"
-                        to={`/blog`}>
-                        <h3 className="h6">Blog</h3>
-                        <p className="">Read what I share</p>
-                    </Link>
-                    <Link
-                        className="p-4 rounded-3 animate__animated  animate__fadeInUp card-delay-1"
-                        to={`/portfolio`}>
-                        <h3 className="h6">Portfolio</h3>
-                        <p>
-                            Projects, experiences knowledge
-                        </p>
-                    </Link>
-                </div>
-                <div className="d-md-flex nav-cards gap-4 mt-4">
-                    <Link
-                        className="p-4 rounded-3 animate__animated  animate__fadeInUp card-delay-2"
-                        to={`/about`}>
-                        <h3 className="h6">About</h3>
-                        <p className="">Read more about me</p>
-                    </Link>
-                    <Link
-                        className="p-4 rounded-3 animate__animated  animate__fadeInUp card-delay-3"
-                        to={`/portfolio`}>
-                        <h3 className="h6">Let's talk</h3>
-                        <p>
-                            Any questions?
-                        </p>
-                    </Link>
-                </div>
-            </section>
+            <Blog />
             <Skills />
         </>
     )
