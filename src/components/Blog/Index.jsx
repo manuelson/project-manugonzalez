dfd7b4045a7561500636527e5a8f858309c43dae
Manu Gonzalez <manuel.gonzalez@interactiv4.com>
2024-01-08 14:52:36 +0100

    font weight 200

--- src/components/Blog/Index.jsx
+++ src/components/Blog/Index.jsx
@@ -6,7 +6,7 @@ export function Blog() {
             <section id="blog" className="container mt-5">
                 <h2 className="h6">Blog</h2>
                 <hr />
-                <div className="d-grid gap-4 blog ">
+                <div className="d-grid gap-3 blog ">
                     <div className="d-flex align-items-center blog-box w-100 justify-content-center">
                         <span className="text-advice">Coming soon</span>
                     </div>
