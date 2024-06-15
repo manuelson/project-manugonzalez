e62280d76d8be32e9bcdbf4ed041fae113238eb6
Manu <manu.gonzalez@vicio.com>
2024-06-15 20:16:27 +0200

    fix post

--- src/components/LastPosts.jsx
+++ src/components/LastPosts.jsx
@@ -1,44 +1,47 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import BlogRepository from "src/service/BlogApi.jsx";
-import {Link} from "react-router-dom";
-import {GetImageAssets} from "../utils/GetImageAssets.jsx";
+import { Link } from "react-router-dom";
+import { GetImageAssets } from "../utils/GetImageAssets.jsx";
 
 export function LastPosts() {
+  const [posts, setPosts] = useState([]);
 
-    const [posts, setPosts] = useState([])
+  useEffect(() => {
+    let blog = new BlogRepository();
+    blog.getLastPosts().then((data) => {
+      if (data) {
+        setPosts(data);
+      }
+    });
+  }, [BlogRepository]);
 
-    useEffect(() => {
-        let blog = new BlogRepository()
-        blog.getLastPosts().then((data) => {
-            setPosts(data)
-        })
-    }, [BlogRepository]);
-
-    return (
-        <>
-            <section id="blog" className="container mt-5">
-                <h2 className="h6">Last Posts</h2>
-                <hr />
-                <div className="d-grid gap-3 blog ">
-                {
-                    posts.map((post, index) => (
-                        <div key={`post-${index}`} className="d-flex align-items-center blog-box w-100 justify-content-center">
-                            <span>
-                                 <Link to={`/post/${post.id}`}>
-                                     <img
-                                         className="blog-image rounded-3"
-                                         src={GetImageAssets(post.image)}
-                                         alt={post.title}
-                                          />
-                                     <h3 className="pt-3">{post.title}</h3>
-                                     <p className="mt-3">{post.resume}</p>
-                                 </Link>
-                            </span>
-                        </div>
-                    ))
-                }
-                </div>
-            </section>
-        </>
-    )
+  console.log(posts);
+  return (
+    <>
+      <section id="blog" className="container mt-5">
+        <h2 className="h6">Last Posts</h2>
+        <hr />
+        <div className="d-grid gap-3 blog ">
+          {posts.map((post, index) => (
+            <div
+              key={`post-${index}`}
+              className="d-flex align-items-center blog-box w-100 justify-content-center"
+            >
+              <span>
+                <Link to={`/post/${post.id}`}>
+                  <img
+                    className="blog-image rounded-3"
+                    src={GetImageAssets(post.image)}
+                    alt={post.title}
+                  />
+                  <h3 className="pt-3">{post.title}</h3>
+                  <p className="mt-3">{post.resume}</p>
+                </Link>
+              </span>
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
 }
