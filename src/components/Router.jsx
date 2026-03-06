344a879f0a93d6a9e833bfcdad1dcc857c16daea
Manu Gonzalez <manuel.gonzalez@uriach.com>
2026-03-06 14:23:06 +0100

    Test

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,30 +1,30 @@
-import { createBrowserRouter } from "react-router-dom"
-import { Layout } from "src/layout/Index.jsx"
+import { createBrowserRouter } from "react-router-dom";
+import { Layout } from "src/layout/Index.jsx";
 import { ErrorPage } from "src/pages/ErrorPage.jsx";
-import {Home} from "src/pages/Home.jsx";
-import {BlogPage} from "src/pages/Blog.jsx";
-import {About} from "src/pages/About.jsx";
+import { Home } from "src/pages/Home.jsx";
+import { BlogPage } from "src/pages/Blog.jsx";
+import { About } from "src/pages/About.jsx";
 
 export const Router = createBrowserRouter([
-    {
-        element: <Layout />,
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                index: "Home",
-                path: "/",
-                element: <Home/>
-            },
-            {
-                index: "Blog",
-                path: '/blog',
-                element: <BlogPage/>
-            },
-            {
-                index: "About",
-                path: '/about',
-                element: <About/>
-            }
-        ],
-    },
+  {
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: "Home",
+        path: "/",
+        element: <Home />,
+      },
+      {
+        index: "Blog",
+        path: "/blog",
+        element: <BlogPage />,
+      },
+      {
+        index: "About",
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
 ]);
